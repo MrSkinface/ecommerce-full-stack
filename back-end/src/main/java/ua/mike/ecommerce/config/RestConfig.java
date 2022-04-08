58d18e315d155b98653ad502060ae6fc05441d68
Mike <levitskym@edin.ua>
2022-04-08 15:07:58 +0300

    expose all primary keys

--- back-end/src/main/java/ua/mike/ecommerce/config/RestConfig.java
+++ back-end/src/main/java/ua/mike/ecommerce/config/RestConfig.java
@@ -8,12 +8,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import ua.mike.ecommerce.models.Category;
 import ua.mike.ecommerce.models.Product;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
 
     private static final HttpMethod[] disabledMethods = {
             HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.PATCH
     };
+
+
+    private final EntityManager entityManager;
+
+    public RestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -25,5 +35,11 @@ public class RestConfig implements RepositoryRestConfigurer {
                 .forDomainType(Category.class)
                 .withItemExposure((meta, methods) -> methods.disable(disabledMethods))
                 .withCollectionExposure((meta, methods) -> methods.disable(disabledMethods));
+        // expose all primary keys
+        config.exposeIdsFor(
+                entityManager.getMetamodel()
+                        .getEntities().stream()
+                        .map(Type::getJavaType).toArray(Class[]::new)
+        );
     }
 }
