9fd8623d085365f119d24e281fdd4999b75cabfc
Mike <levitskym@edin.ua>
2022-04-08 21:20:10 +0300

    close #9

---
+++ front-end/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Category} from "../../common/category";
+
+@Component({
+  selector: 'product-category-menu',
+  templateUrl: './product-category-menu.component.html',
+  styleUrls: ['./product-category-menu.component.css']
+})
+export class ProductCategoryMenuComponent implements OnInit {
+
+  categories: Category[] = [];
+  constructor(private products: ProductService) { }
+
+  ngOnInit(): void {
+    this.products.categories().subscribe(data => this.categories = data)
+  }
+
+}
--- front-end/src/app/services/product.service.ts
+++ front-end/src/app/services/product.service.ts
@@ -2,25 +2,38 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 import {Product} from "../common/product";
+import {Category} from "../common/category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private url: string = "http://localhost:8080/api/products";
+  private url: string = "http://localhost:8080/api";
 
   constructor(private http: HttpClient) { }
 
   listForCategory(categoryID: number): Observable<Product[]> {
-    return this.http.get<GetResponse>(`${this.url}/search/findByCategoryId?id=${categoryID}`).pipe(
+    return this.http.get<GetProductsResponse>(`${this.url}/products/search/findByCategoryId?id=${categoryID}`).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  categories(): Observable<Category[]> {
+    return this.http.get<GetCategoriesResponse>(`${this.url}/product-category`).pipe(
+      map(response => response._embedded.categories)
     );
   }
 }
 
-interface GetResponse {
+interface GetProductsResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetCategoriesResponse {
+  _embedded: {
+    categories: Category[];
   }
 }
