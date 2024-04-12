5408c4172a8d72fe18134fe23ae8bc715d94b753
Mike Levitsky <michael.levitsky@trueplay.io>
2024-04-12 08:57:21 +0300

    ...

---
+++ back-end/src/test/java/ua/mike/ecommerce/controllers/PurchaseControllerTest.java
@@ -0,0 +1,28 @@
+package ua.mike.ecommerce.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Created by mike on 12.04.2024 08:55
+ */
+@ExtendWith(MockitoExtension.class)
+class PurchaseControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void purchase() {
+    }
+
+    @Test
+    void paymentIntent() {
+    }
+}
