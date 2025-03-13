59ed6d40737af44bda1ac6615d175218c413e0b2
Mike Levitsky <michael.levitsky@trueplay.io>
2025-03-13 10:07:22 +0200

    Add cache

--- back-end/src/main/java/ua/mike/ecommerce/persistence/repository/ProductRepository.java
+++ back-end/src/main/java/ua/mike/ecommerce/persistence/repository/ProductRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
+import ua.mike.ecommerce.config.CacheNames;
 import ua.mike.ecommerce.persistence.entity.Product;
 
 import java.util.Optional;
 
-@CacheConfig(cacheNames = "product")
+@CacheConfig(cacheNames = CacheNames.PRODUCTS)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Override @NonNull @Cacheable
