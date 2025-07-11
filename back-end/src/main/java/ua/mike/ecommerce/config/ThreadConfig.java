2d79f398741db61cede24e129c862f36473a1914
Mike Levitsky <michael.levitsky@trueplay.io>
2025-07-11 15:41:27 +0300

    Update SpringBoot + virtual threads

---
+++ back-end/src/main/java/ua/mike/ecommerce/config/ThreadConfig.java
@@ -0,0 +1,26 @@
+package ua.mike.ecommerce.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
+import org.springframework.boot.autoconfigure.thread.Threading;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+@ConditionalOnThreading(Threading.VIRTUAL)
+public class ThreadConfig {
+
+    @Bean
+    public AsyncTaskExecutor applicationTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
